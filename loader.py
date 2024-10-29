a5c818ea64bf90f8c5882d5105e590963f9765a8
MoziL <57535353+simozil@users.noreply.github.com>
2024-10-29 17:42:11 +0700

    Update loader.py

--- loader.py
+++ loader.py
@@ -4,10 +4,13 @@ from core.solvers import *
 from utils import load_config, FileOperations
 
 config = load_config()
+
+# Initialize the captcha solver based on the selected captcha module
 captcha_solver = (
-    AntiCaptchaImageSolver(config.anti_captcha_api_key)
-    if config.captcha_module == "anticaptcha"
-    else TwoCaptchaImageSolver(config.two_captcha_api_key)
+    TwoCaptchaImageSolver(config.two_captcha_api_key)
+    if config.captcha_module == "2captcha"
+    else AntiCaptchaImageSolver(config.anti_captcha_api_key)
 )
+
 file_operations = FileOperations()
 semaphore = asyncio.Semaphore(config.threads)
